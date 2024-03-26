99180ff4f9054409fd5fd3063ff03321b60cb9e3
Кіт <@DESKTOP-ENENKMM>
2024-03-26 20:00:23 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static string n;    //cipher
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo keyexit;
+            string cipher = "Input words for code.";
+            string uncipher = "Input words for uncode.";
+
+            var cscipher = new Cscipher();
+
+            do
+            {
+                Console.WriteLine($"Press N to {cipher.Remove(cipher.Length-1)}, " +
+                    $"M to {uncipher.Remove(22)} or " +
+                    "press Escape to close.");
+                keyexit = Console.ReadKey(true);
+                if(keyexit.Key == ConsoleKey.N)
+                {
+                    Console.Clear();
+                    Console.WriteLine(cipher);
+                    n = Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine(n);
+
+                    cscipher.Cipher();  //cipher (cipher.cs)
+
+                    //end of cipher
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Pressed wrong key, try again.");
+                }
+
+            } while (keyexit.Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ ConsoleApp1/cipher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Cscipher
+    {
+        public void Cipher()
+        {
+            string k = Program.n;
+            int namount = k.Length;
+            int namount1 = 0;
+            var aramount = new char[namount];
+
+            for (int i = 0; i < aramount.Length; i++)
+            {
+                if (k[i] == 'z')
+                {
+                    namount1 = k.IndexOf("z");
+                    Console.Write("{0}", k.Substring('a'));
+                }
+                aramount[i] = Convert.ToChar(k[i] + 3);
+            }
+            Console.WriteLine(new string(aramount));
+
+            /*string k = Program.n;
+            int lenghtMoving = 3;
+            k = string.Format("{0}{1}", k.Substring(k.Length - lenghtMoving),
+                k.Substring(0, k.Length - lenghtMoving));
+            Console.WriteLine(k);*/
+        }
+    }
+}
